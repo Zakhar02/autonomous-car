6511bf2a9fe045d52d05c88a4526dd922ad807cf
Zakhar Yagudin <assasinscreed.ru01@gmail.com>
2023-04-06 14:56:39 +0300

    minor fix

--- MPC_SIM.py
+++ MPC_SIM.py
@@ -51,7 +51,7 @@ def main():
     print(f"MPC calculation time is {-time1} seconds")
     fig = plt.figure()
     animation = FuncAnimation(
-        fig, plot_car, frames=N+1, fargs=(.5, l, xs, state))
+        fig, plot_car, frames=N+1, fargs=(l/2, l, xs, state))
     animation.save('mpc.gif', writer='imagemagick', fps=60)
 
 
