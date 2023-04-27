10f4c3f85e19445f2b065a4d8d7c234f1d3795fa
Zakhar Yagudin <assasinscreed.ru01@gmail.com>
2023-04-27 15:47:22 +0300

    add u_ref to MPC

--- MPC_SIM.py
+++ MPC_SIM.py
@@ -41,11 +41,13 @@ def main():
     state_initial = [0, 0, 0]
     state_final = [30, 30, np.pi/2]
     df = DifferentialFlatness(l)
-    state, _ = df.build_trajectory(state_initial, state_final, N+1, 100)
+    state, u_ref = df.build_trajectory(state_initial, state_final, N+1, 100)
     state_f = np.tile(state[:, -1], (H+1, 1)).T
     state = np.vstack((state.T, state_f.T)).T
+    u_f = np.tile(u_ref[:, -1], (H-1, 1)).T
+    u_ref = np.vstack((u_ref.T, u_f.T)).T
     Q = 60*np.eye(3)
-    R = np.array([[1, 0], [0, 10]])
+    R = np.array([[1, 0], [0, 1]])
     P = 120*np.eye(3)
     xs = np.array([1, 1, 0]).reshape(1, 3)
     us = np.array([0, 0]).reshape(1, 2)
@@ -56,7 +58,7 @@ def main():
     time1 = time.time()
     for i in range(N):
         _, u = nmpc.solver()(xs[-1], state[:, i:i+H+1],
-                             dt*H, np.zeros(2), Q, R, P, 30, np.pi/4, 3, 1)
+                             dt*H, u_ref[:, i:i+H], Q, R, P, 30, np.pi/4, 3, 1)
         vi, phii = [CubicSpline(np.linspace(0, dt*H, H), u.full()[i, :].ravel())
                     for i in range(u.shape[0])]
         for u_ in zip(vi(np.linspace(0, dt, n)), phii(np.linspace(0, dt, n))):
