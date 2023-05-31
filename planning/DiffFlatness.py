4e4af61258687772918643aec4c234bd0b7540c9
Zakhar Yagudin <assasinscreed.ru01@gmail.com>
2023-05-31 18:54:57 +0300

    update planning

--- planning/DiffFlatness.py
+++ planning/DiffFlatness.py
@@ -5,22 +5,22 @@ import numpy as np
 class DifferentialFlatness:
     def __init__(self, l):
         self.l = l
-        self.t = sp.symbols('t')
+        t = sp.symbols('t')
         xi = sp.symbols("xi")
         xf = sp.symbols("xf")
         alpha = sp.symbols("alpha")
         beta = sp.symbols("beta")
-        x = self.trajectory()(xi, xf, alpha, beta, self.t)
-        dx = x.diff(self.t)
-        ddx = dx.diff(self.t)
-        self.syms = [sp.lambdify([xi, xf, alpha, beta], x_)
-                     for x_ in [x, dx, ddx]]
+        phi_s = self.trajectory()(xi, xf, alpha, beta, t)
+        dphi_s = phi_s.diff(t)
+        ddphi_s = dphi_s.diff(t)
+        self.syms = [sp.lambdify([xi, xf, alpha, beta, t], func_)
+                     for func_ in [phi_s, dphi_s, ddphi_s]]
+        self.phi = self.syms[0]
+        self.dphi = self.syms[1]
+        self.ddphi = self.syms[2]
 
     def trajectory(self):
         return lambda xi, xf, alpha, beta, s: s**3*xf - (s - 1)**3*xi + alpha*s**2*(s - 1) + beta*s*(s - 1)**2
-
-    def sym2f(self, sym, xi, xf, alpha, beta):
-        return [f(xi, xf, alpha, beta) for f in sym]
 
     def build_trajectory(self, state_initial, state_final, N, k=1):
         xi, yi, thetai = state_initial
@@ -29,11 +29,13 @@ class DifferentialFlatness:
         alphay = k*np.sin(thetaf) - 3*yf
         betax = k*np.cos(thetai) + 3*xi
         betay = k*np.sin(thetai) + 3*yi
-        x, dx, ddx = self.sym2f(self.syms, xi, xf, alphax, betax)
-        y, dy, ddy = self.sym2f(self.syms, yi, yf, alphay, betay)
-        t_ = np.linspace(0, 1, N)
-        x, y, dx, dy, ddx, ddy = [np.fromiter(map(sp.lambdify(
-            self.t, f), t_), dtype=np.double) for f in [x, y, dx, dy, ddx, ddy]]
+        t = np.linspace(0, 1, N)
+        x = self.phi(xi, xf, alphax, betax, t)
+        y = self.phi(yi, yf, alphay, betay, t)
+        dx = self.dphi(xi, xf, alphax, betax, t)
+        dy = self.dphi(yi, yf, alphay, betay, t)
+        ddx = self.ddphi(xi, xf, alphax, betax, t)
+        ddy = self.ddphi(yi, yf, alphay, betay, t)
         thetad = np.arctan2(dy, dx)
         vd = np.sqrt(np.square(dx)+np.square(dy))
         dthetad = np.divide(ddy*dx-ddx*dy, np.square(dx)+np.square(dy))
