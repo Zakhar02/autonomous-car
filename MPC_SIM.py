a175dea0f281b59389d2a740145af1c9114e2d58
Zakhar Yagudin <assasinscreed.ru01@gmail.com>
2023-05-31 19:34:24 +0300

    tuning

--- MPC_SIM.py
+++ MPC_SIM.py
@@ -7,7 +7,7 @@ import time
 from scipy.interpolate import CubicSpline
 
 
-def plot_car(i, w, l, xs, us, state, n):
+def plot_car(i, w, l, xs, us, state, n, r, rad):
     plt.cla()
     i *= n
     x, y, theta = xs[i]
@@ -27,10 +27,12 @@ def plot_car(i, w, l, xs, us, state, n):
     plt.xlabel('x')
     plt.ylabel('y')
     plt.plot(xs[:, 0], xs[:, 1], label="FMPC")
-    plt.plot(state[0, :], state[1, :], label="Flat Output")
+    plt.plot(state[0, :], state[1, :], label="Desired Trajectory")
     plt.plot(car[0, :] + x, car[1, :] + y, label="Car")
     plt.quiver(*origin, vv[0], vv[1], color="blue", label="$v$")
     plt.quiver(*origin, vphi[0], vphi[1], color="red", label="$\phi$")
+    circ = plt.Circle(r, rad, color='g')
+    plt.gca().add_patch(circ)
     plt.legend()
 
 
@@ -39,26 +41,27 @@ def main():
     N = 100
     l = 1
     state_initial = [0, 0, 0]
-    state_final = [30, 30, np.pi/2]
+    state_final = [30, 30, 0]
     df = DifferentialFlatness(l)
     state, _ = df.build_trajectory(state_initial, state_final, N, 100)
     state_f = np.tile(state[:, -1], (H+1, 1)).T
     state = np.vstack((state.T, state_f.T)).T
-    Q = 100*np.eye(3)
-    R = np.array([[1, 0], [0, 1]])
-    P = 120*np.eye(3)
     xs = np.array([1, 1, 0]).reshape(1, 3)
     us = np.array([0, 0]).reshape(1, 2)
-    nmpc = FMPC(H, l)
     tf = 10
     dt = tf/N
     n = 5
-    r = np.array([10, 0])
+    r = np.array([5, 0])
+    rad = 2
     time1 = time.time()
+    nmpc = FMPC(H, l)
+    Q = 220*np.eye(3)
+    R = np.array([[1, 0], [0, 1]])
+    P = 180*np.eye(3)
     for i in range(N):
         x_ref, u_ref = df.build_trajectory(xs[-1], state[:, i+H+1], H+1)
         _, u = nmpc.solver()(xs[-1], x_ref,
-                             dt*H, u_ref[:, :-1], Q, R, P, 30, np.pi/4, 3, 1, r, 1)
+                             dt*H, u_ref[:, :-1], Q, R, P, 30, np.pi/4, 3, 1, r, rad)
         vi, phii = [CubicSpline(np.linspace(0, dt*H, H), u.full()[i, :].ravel())
                     for i in range(u.shape[0])]
         for u_ in zip(vi(np.linspace(0, dt, n)), phii(np.linspace(0, dt, n))):
@@ -69,9 +72,9 @@ def main():
     print(f"MPC calculation time is {-time1} seconds")
     fig = plt.figure()
     animation = FuncAnimation(
-        fig, plot_car, frames=N+1, fargs=(l/2, l, xs, us, state, n))
-    # animation.save('mpc.gif', writer='imagemagick', fps=60)
-    plt.show()
+        fig, plot_car, frames=N+1, fargs=(l/2, l, xs, us, state, n, r, rad))
+    animation.save('mpc.gif', writer='imagemagick', fps=60)
+    # plt.show()
 
 
 if __name__ == "__main__":
